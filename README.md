d385a2c156ad8d52bab9dd642061ee8f279eb8e5
Michael Binder <rednib.coding@gmail.com>
2024-07-16 18:48:55 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ImgToC
 Simple but awesome Image to C array converter written in plain html/js/css
+
+![](doc/screenshot.png)
+
+## Try it out
+Visit: [imgtoc online](https://imgtoc.netlify.app/)
---
+++ src/script.js
@@ -0,0 +1,117 @@
+let fullCArray = '';
+
+document.getElementById('imageInput').addEventListener('change', handleFileSelect);
+
+function handleFileSelect(event) {
+    const file = event.target.files[0];
+    displayThumbnail(file);
+}
+
+function displayThumbnail(file) {
+    const thumbnail = document.getElementById('thumbnail');
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            thumbnail.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    } else {
+        thumbnail.src = 'placeholder.png';
+    }
+}
+
+document.getElementById('convertButton').addEventListener('click', function () {
+    const fileInput = document.getElementById('imageInput');
+    const file = fileInput.files[0];
+    const format = document.getElementById('formatSelect').value;
+    if (!file) {
+        alert('Please select an image file.');
+        return;
+    }
+
+    const spinnerContainer = document.getElementById('spinnerContainer');
+    spinnerContainer.style.display = 'block';
+
+    const reader = new FileReader();
+    reader.onload = function (event) {
+        const img = new Image();
+        img.onload = function () {
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+
+            const imageData = ctx.getImageData(0, 0, img.width, img.height);
+            const data = imageData.data;
+            fullCArray = 'unsigned char image[] = {\n';
+
+            for (let i = 0; i < data.length; i += 4) {
+                switch (format) {
+                    case 'RGBA8888':
+                        fullCArray += `0x${data[i].toString(16).padStart(2, '0')}, `;
+                        fullCArray += `0x${data[i + 1].toString(16).padStart(2, '0')}, `;
+                        fullCArray += `0x${data[i + 2].toString(16).padStart(2, '0')}, `;
+                        fullCArray += `0x${data[i + 3].toString(16).padStart(2, '0')},\n`;
+                        break;
+                    case 'ARGB8888':
+                        fullCArray += `0x${data[i + 3].toString(16).padStart(2, '0')}, `;
+                        fullCArray += `0x${data[i].toString(16).padStart(2, '0')}, `;
+                        fullCArray += `0x${data[i + 1].toString(16).padStart(2, '0')}, `;
+                        fullCArray += `0x${data[i + 2].toString(16).padStart(2, '0')},\n`;
+                        break;
+                    case 'RGB565A8':
+                        let rgb565 = ((data[i] & 0xf8) << 8) | ((data[i + 1] & 0xfc) << 3) | (data[i + 2] >> 3);
+                        fullCArray += `0x${(rgb565 >> 8).toString(16).padStart(2, '0')}, `;
+                        fullCArray += `0x${(rgb565 & 0xff).toString(16).padStart(2, '0')}, `;
+                        fullCArray += `0x${data[i + 3].toString(16).padStart(2, '0')},\n`;
+                        break;
+                    case 'RGB565':
+                        let rgb565only = ((data[i] & 0xf8) << 8) | ((data[i + 1] & 0xfc) << 3) | (data[i + 2] >> 3);
+                        fullCArray += `0x${(rgb565only >> 8).toString(16).padStart(2, '0')}, `;
+                        fullCArray += `0x${(rgb565only & 0xff).toString(16).padStart(2, '0')},\n`;
+                        break;
+                    case 'RGB888':
+                        fullCArray += `0x${data[i].toString(16).padStart(2, '0')}, `;
+                        fullCArray += `0x${data[i + 1].toString(16).padStart(2, '0')}, `;
+                        fullCArray += `0x${data[i + 2].toString(16).padStart(2, '0')},\n`;
+                        break;
+                }
+            }
+
+            fullCArray = fullCArray.replace(/,\n$/, '\n};');
+            document.getElementById('cArrayOutput').value = fullCArray.length > 10000 ? fullCArray.substring(0, 10000) + '...\n' + '...' : fullCArray;
+            spinnerContainer.style.display = 'none';
+        };
+
+        img.src = event.target.result;
+    };
+
+    reader.readAsDataURL(file);
+});
+
+document.getElementById('copyButton').addEventListener('click', function () {
+    navigator.clipboard.writeText(fullCArray);
+    alert('C array copied to clipboard!');
+});
+
+document.getElementById('thumbnailContainer').addEventListener('dragover', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.classList.add('dragging');
+});
+
+document.getElementById('thumbnailContainer').addEventListener('dragleave', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.classList.remove('dragging');
+});
+
+document.getElementById('thumbnailContainer').addEventListener('drop', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.classList.remove('dragging');
+    const file = e.dataTransfer.files[0];
+    document.getElementById('imageInput').files = e.dataTransfer.files;
+    displayThumbnail(file);
+});
